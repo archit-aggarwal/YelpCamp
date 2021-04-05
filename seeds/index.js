21a306b99947667f7550c61a8caa481a620cd62e
archit-aggarwal <archit.aggarwal023@gmail.com>
2021-04-05 15:10:04 +0530

    Added Campground Model

---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+// Seeding is filling a Database with a set of initial dummy records.
+// It needs to be done only once by calling 'node seeds/index.js' from terminal.
+
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+// Insert Random Dummy Campgrounds into MongoDB (to start application with dummy data)
+// (If not seeded, initially database will be empty which is difficult to work with)
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
